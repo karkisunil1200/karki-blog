40d79f67d96f7e000caa11308804c93fc4d86534
sunilkarki <karkisunil1200@gmail.com>
2022-02-13 08:24:11 -0600

    Added topbar componenet

--- src/topbar/Topbar.jsx
+++ src/topbar/Topbar.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import "./topbar.css";
 
 const Topbar = () => {
-  return <div>Topbar</div>;
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fa-brands fa-facebook-square"></i>
+        <i className="topIcon fa-brands fa-twitter-square"></i>
+        <i className="topIcon fa-brands fa-instagram-square"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">lOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img
+          src="https://avatarfiles.alphacoders.com/123/123497.png"
+          alt="profile-pic"
+          className="topImg"
+        />
+        <i class="topSearchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
+  );
 };
-
 export default Topbar;
