1538512f41ee98de7a7ee32da583c52a640cc0f6
sunilkarki <karkisunil1200@gmail.com>
2022-02-13 16:50:41 -0600

    Added routes

--- src/App.js
+++ src/App.js
@@ -6,13 +6,36 @@ import Write from "./pages/write/Write";
 import Setting from "./pages/settings/Setting";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
+  const user = false;
+
   return (
-    <div>
+    <Router>
       <Topbar />
-      <Register />
-    </div>
+      <Routes>
+        <Route path="/" element={user ? <Home /> : <Register />} />
+        <Route exact path="/login" element={user ? <Home /> : <Login />} />
+        <Route
+          exact
+          path="/register"
+          element={user ? <Home /> : <Register />}
+        />
+        <Route exact path="/write" element={user ? <Write /> : <Register />} />
+        <Route
+          exact
+          path="/post/:postId"
+          element={user ? <Single /> : <Register />}
+        />
+
+        <Route
+          exact
+          path="/setting"
+          element={user ? <Setting /> : <Register />}
+        />
+      </Routes>
+    </Router>
   );
 };
 
