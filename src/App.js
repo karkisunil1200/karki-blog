7270b226d980319347f6404fd60939009d3aad33
sunilkarki <karkisunil1200@gmail.com>
2022-02-13 14:16:23 -0600

    Added input texts for write component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import Home from "./pages/home/Home";
 import Topbar from "./components/topbar/Topbar";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 const App = () => {
   return (
     <div>
       <Topbar />
-      <Single />
+      <Write />
     </div>
   );
 };
---
+++ src/pages/write/Write.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./write.css";
+
+const Write = () => {
+  return (
+    <div className="write">
+      <form className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+            <i className=" writeIcon fa-solid fa-plus"></i>
+          </label>
+          <input type="file" id="fileInput" style={{ display: "none" }} />
+          <input
+            type="text"
+            placeholder="Title"
+            className="writeInput"
+            autoFocus={true}
+          />
+        </div>
+
+        <div className="writeFormGroup">
+          <textarea
+            placeholder="Tell your story...."
+            type="text"
+            className="writeInput writeText"
+          ></textarea>
+          <button className="writeSubmit">Publish</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Write;
